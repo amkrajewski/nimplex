6501d09eda67592ad699889bb882f97a11bf7ebf
amkrajewski <ak@psu.edu>
2023-08-10 18:08:18 -0700

    - added internal simplex grid generator and tested it :)

--- nimplex.nim
+++ nimplex.nim
@@ -1,6 +1,6 @@
 # Copyrigth (C) 2023 Adam M. Krajewski
 
-import std/math
+from std/math import binom
 import std/sugar
 import arraymancer
 import strutils
@@ -8,6 +8,7 @@ import strutils
 proc simplex_grid*(dim: int, 
                    ndiv: int): Tensor[int] =
 
+    # L is the total number of unique points in the simplex grid, which we know a priori
     let L: int = binom(ndiv+dim-1, dim-1)
     result = newTensor[int]([L, dim])
     var x = zeros[int](dim)
@@ -35,6 +36,28 @@ proc simplex_grid_fractional*(dim: int,
     result = result.map(x => x / float(ndiv))
     return result
 
+proc simplex_internal_grid*(dim: int, 
+                            ndiv: int): Tensor[int] =
+
+    # L is the total number of unique points inside the simplex grid, which we know a priori
+    let L: int = binom(ndiv-1, dim-1)
+    result = newTensor[int]([L, dim])
+    var x = ones[int](dim)
+    x[dim-1] = ndiv+1-dim
+    for j in 0..dim-1:
+        result[0, j] = x[j]
+    var h = dim
+    for i in 1..L-1:
+        h -= 1
+        let val = x[h]
+        x[h] = 1
+        x[dim-1] = val - 1
+        x[h-1] += 1
+        for j in 0..dim-1:
+            result[i, j] = x[j]
+        if val != 2:
+            h = dim
+    return result
 
 echo "Simplex dimensions:"
 let dim = readLine(stdin).parseInt() 
@@ -42,4 +65,6 @@ let dim = readLine(stdin).parseInt()
 echo "N divisions:"
 let ndiv = readLine(stdin).parseInt() 
 
-echo simplex_grid(dim, ndiv)
+let temp = simplex_internal_grid(dim, ndiv)
+echo "Internal grid:", temp
+echo "Internal grid size:", temp.shape[0]
