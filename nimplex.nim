388dec931ad83f4d5ed5cdf053f324b9aedb1468
amkrajewski <ak@psu.edu>
2023-11-13 16:21:35 -0500

    - 4.5D happy

--- nimplex.nim
+++ nimplex.nim
@@ -142,15 +142,18 @@ proc simplex_graph3_nim(
     var x = zeros[int](dim)
 
     func neighborsLink(i:int, x:Tensor): seq[int] =
+        let jump0 = 1
+        let jump1 = binom(1+12-x[0], 1)
+
         if x[0] != 0:
-            result.add(i-binom(1+12-x[0], 1))
-            result.add(i-1-binom(1+12-x[0], 1) )
+            result.add(i-jump1)
+            result.add(i-jump1-jump0)
         if x[1] != 0:
-            result.add(i-1)
-            result.add(i-1+binom(1+12-x[0], 1))
+            result.add(i-jump0)
+            result.add(i+jump1-jump0)
         if x[2] != 0:
-            result.add(i+1)
-            result.add(i+binom(1+12-x[0], 1))
+            result.add(i+jump0)
+            result.add(i+jump1)
         return result
 
     x[dim-1] = ndiv
@@ -197,34 +200,28 @@ proc simplex_graph4_nim(
     var x = zeros[int](dim)
 
     func neighborsLink(i:int, x:Tensor): seq[int] =
+        let jump0 = 1  #binom(12-x[0]-x[1]-x[2], 0)
         let jump1 = binom(1+12-x[0]-x[1], 1)
         let jump2 = binom(2+12-x[0], 2)
         if x[0] != 0:
-            result.add(i - jump2 - jump1)
-            result.add(i-1-jump2 - jump1)
-            result.add(i - jump2)
+            result.add(i - jump2)                 # quaternary
+            result.add(i - jump2 - jump1)         # quaternary
+            result.add(i - jump2 - jump1 - jump0) # quaternary
 
         if x[1] != 0:
-            result.add(i-jump1)             # ternary
-            result.add(i-1-jump1)           # ternary
-            result.add(i+jump2-jump1-x[1])  # quaternary
+            result.add(i - jump1)                 # ternary
+            result.add(i - jump1 - jump0)         # ternary
+            result.add(i + jump2 - jump1 - x[1])  # quaternary
 
         if x[2] != 0:
-            result.add(i-1)                 # binary
-            result.add(i-1+jump1)           # ternary
-            result.add(i-1+jump2-x[1])      # quaternary
-        if x[3] != 0:
-            result.add(i+1)                 # binary
-            result.add(i+jump1)             # ternary
-            result.add(i+jump2-x[1])        # quaternary
+            result.add(i - jump0)                 # binary
+            result.add(i + jump1 - jump0)         # ternary
+            result.add(i + jump2 - jump0 - x[1])  # quaternary
 
-        
-        #if x[1] != 0:
-        #    result.add(i-1)
-        #    
-        #if x[2] != 0:
-        #    result.add(i+1)
-        #    
+        if x[3] != 0:
+            result.add(i + jump0)                 # binary
+            result.add(i + jump1)                 # ternary
+            result.add(i + jump2 - x[1])          # quaternary  
 
         return result
 
@@ -252,4 +249,67 @@ proc simplex_graph4_nim(
 
 proc simplex_graph4*(dim: int, ndiv: int): (seq[seq[int]], seq[seq[int]]) {.exportpy.} = 
     let graph = simplex_graph4_nim(dim, ndiv)
+    return (graph[0].toSeq2D(), graph[1])
+
+proc simplex_graph5_nim(
+    dim: int, 
+    ndiv: int): (Tensor[int], seq[seq[int]]) =
+
+    # L is the total number of unique points in the simplex grid, which we know a priori
+    let L: int = binom(ndiv+dim-1, dim-1)
+    var result1 = newTensor[int]([L, dim])
+    var neighbors = newSeq[seq[int]](L)
+    var x = zeros[int](dim)
+
+    func neighborsLink(i:int, x:Tensor): seq[int] =
+        let jump0 = 1  #binom(12-x[0]-x[1]-x[2]-x[3], 0)
+        let jump1 = binom(1+12-x[0]-x[1]-x[2], 1)
+        let jump2 = binom(2+12-x[0]-x[1], 2)
+
+        if x[1] != 0:
+            result.add(i - jump2)                 # quaternary
+            result.add(i - jump2 - jump1)         # quaternary
+            result.add(i - jump2 - jump1 - jump0) # quaternary
+
+        if x[2] != 0:
+            result.add(i - jump1)                 # ternary
+            result.add(i - jump1 - jump0)         # ternary
+            result.add(i + jump2 - jump1 - x[2])  # quaternary
+
+        if x[3] != 0:
+            result.add(i - jump0)                 # binary
+            result.add(i + jump1 - jump0)         # ternary
+            result.add(i + jump2 - jump0 - x[2])  # quaternary
+
+        if x[4] != 0:
+            result.add(i + jump0)                 # binary
+            result.add(i + jump1)                 # ternary
+            result.add(i + jump2 - x[2])          # quaternary  
+
+        return result
+
+    x[dim-1] = ndiv
+    for j in 0..dim-1:
+        result1[0, j] = x[j]
+    var h = dim
+
+    neighbors[0] = neighborsLink(0, x)
+    #echo x.toSeq1D(), neighbors[0], "\n"
+
+    for i in 1..L-1:
+        h -= 1
+        let val = x[h]
+        x[h] = 0
+        x[dim-1] = val - 1
+        x[h-1] += 1
+        for j in 0..dim-1:
+            result1[i, j] = x[j]
+        neighbors[i] = neighborsLink(i, x)
+        #echo x.toSeq1D(), neighbors[i],  "\n"
+        if val != 1:
+            h = dim
+    return (result1, neighbors)
+
+proc simplex_graph5*(dim: int, ndiv: int): (seq[seq[int]], seq[seq[int]]) {.exportpy.} = 
+    let graph = simplex_graph5_nim(dim, ndiv)
     return (graph[0].toSeq2D(), graph[1])
