cdc62cb89320d613be4d61daf1dc6e1df3859278
amkrajewski <ak@psu.edu>
2024-01-10 13:08:41 +0100

    - added test for small ternary graph corectness

--- tests/graph.nim
+++ tests/graph.nim
@@ -25,3 +25,33 @@ suite "small simplex integer 2-component (binary) graph":
     test "correct neighbors list for each node/vertex":
         check neighbors == 
             @[@[1], @[0, 2], @[1, 3], @[2, 4], @[3, 5], @[4]]
+
+suite "small simplex integer 3-component (ternary) graph":
+    let (nodes, neighbors) = nimplex.simplex_graph(3, 3)
+    var neighborsNumber: seq[int]
+    for i in 0..5:
+        neighborsNumber.add(neighbors[i].len)
+        
+    echo "Nodes:\n", nodes
+    echo "Neighbors:\n", neighbors
+
+    test "correct dimensionality of nodes/vertices":
+        check nodes.shape[1] == 3
+    test "correct number of nodes/vertices":
+        check nodes.shape[0] == 10
+    test "correct maximum number of neighbors":
+        check neighborsNumber.max == 3*(3-1)
+    test "correct minimum number of neighbors":
+        check neighborsNumber.min == (3-1)
+    test "correct node/vertex positions in the simplex":
+        check nodes.toSeq2D() == @[
+            @[0, 0, 3], @[0, 1, 2], @[0, 2, 1], @[0, 3, 0], 
+            @[1, 0, 2], @[1, 1, 1], @[1, 2, 0], 
+            @[2, 0, 1], @[2, 1, 0], 
+            @[3, 0, 0]]
+    test "correct neighbors list for each node/vertex":
+        check neighbors == @[
+            @[1, 4], @[0, 2, 4, 5], @[1, 3, 5, 6], @[2, 6], 
+            @[1, 0, 5, 7], @[2, 1, 4, 6, 7, 8], @[3, 2, 5, 8], 
+            @[5, 4, 8, 9], @[6, 5, 7, 9], 
+            @[8, 7]]
