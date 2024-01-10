0d9f6135aa06cf5b71360f0ad35038c28c3a1aa8
amkrajewski <ak@psu.edu>
2024-01-10 12:37:09 +0100

    - added tests for large (500k+ points) grids

--- tests/grid.nim
+++ tests/grid.nim
@@ -1,7 +1,7 @@
 import std/unittest
 import ../nimplex
 import arraymancer/Tensor
-import math
+
 
 suite "small simplex full integer grids":
     let result = nimplex.simplex_grid(2, 5)
@@ -14,7 +14,6 @@ suite "small simplex full integer grids":
     test "correct values":
         check result.toSeq2D() == 
             @[@[0, 5], @[1, 4], @[2, 3], @[3, 2], @[4, 1], @[5, 0]]
-        
 
 suite "small simplex internal integer grids":
     let result = nimplex.simplex_internal_grid(2, 7)
@@ -37,6 +36,7 @@ suite "small simplex full fractional grids":
     test "grid has correct number of nodes/vertices":
         check result.shape[0] == 6
     test "correct values":
+        # Nice Finite Decimal Fractions we can check for exact equality directly
         check result.toSeq2D() == 
             @[@[0.0, 1.0], @[0.2, 0.8], @[0.4, 0.6], @[0.6, 0.4], @[0.8, 0.2], @[1.0, 0.0]]
 
@@ -49,6 +49,7 @@ suite "small simplex internal fractional grids":
     test "grid has correct number of vertices":
         check result.shape[0] == 6
     test "correct values":
+        # Infinite Decimal Fractions, so we check for approximate equality
         let reference = @[
             @[0.142857, 0.857142], 
             @[0.285714, 0.714285], 
@@ -59,3 +60,57 @@ suite "small simplex internal fractional grids":
         for i in 0..5:
             for j in 0..1:
                 check abs(result[i, j] - reference[i][j]) < 0.0001
+
+suite "large simplex full integer grids":
+    let result = nimplex.simplex_grid(7, 24)
+    let resultCherryPick = result[124_492, _].toSeq2D()[0]
+    echo "Shape:  ", result.shape
+    echo "Result #124_492:  ", resultCherryPick
+    test "correct dimensionality":
+        check result.shape[1] == 7
+    test "grid has correct number of nodes/vertices":
+        check result.shape[0] == 593_775
+    test "correct value at index 124_492":
+        check resultCherryPick == @[1, 0, 2, 3, 9, 8, 1]
+    
+suite "small simplex internal integer grids":
+    let result = nimplex.simplex_internal_grid(7, 24)
+    let resultCherryPick = result[44_234, _].toSeq2D()[0]
+    echo "Shape:  ", result.shape
+    echo "Result #44_234:  ", resultCherryPick
+    test "correct dimensionality":
+        check result.shape[1] == 7
+    test "grid has correct number of vertices":
+        check result.shape[0] == 100_947
+    test "correct value at index 44_234":
+        check resultCherryPick == @[2, 7, 3, 2, 1, 4, 5]
+
+suite "large simplex full fractional grids":
+    let result = nimplex.simplex_grid_fractional(7, 24)
+    let resultCherryPick = result[124_492, _].toSeq2D()[0]
+    echo "Shape:  ", result.shape
+    echo "Result #124_492:  ", resultCherryPick
+    test "correct dimensionality":
+        check result.shape[1] == 7
+    test "grid has correct number of nodes/vertices":
+        check result.shape[0] == 593_775
+    test "correct value at index 124_492":
+        # Infinite Decimal Fractions, so we check for approximate equality
+        let reference = @[0.041666, 0.0, 0.083333, 0.125, 0.375, 0.333333, 0.041666]
+        for i in 0..6:
+            check abs(resultCherryPick[i] - reference[i]) < 0.0001
+
+suite "large simplex internal fractional grids":
+    let result = nimplex.simplex_internal_grid_fractional(7, 24)
+    let resultCherryPick = result[44_234, _].toSeq2D()[0]
+    echo "Shape:  ", result.shape
+    echo "Result #44_234:  ", resultCherryPick
+    test "correct dimensionality":
+        check result.shape[1] == 7
+    test "grid has correct number of vertices":
+        check result.shape[0] == 100_947
+    test "correct value at index 44_234":
+        # Infinite Decimal Fractions, so we check for approximate equality
+        let reference = @[0.0833333, 0.291666, 0.125, 0.0833333, 0.0416666, 0.166666, 0.208333]
+        for i in 0..6:
+            check abs(resultCherryPick[i] - reference[i]) < 0.0001
