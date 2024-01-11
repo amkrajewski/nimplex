e2913783b52589258f5f698fd8d32298a736b436
amkrajewski <ak@psu.edu>
2024-01-11 18:10:43 +0100

    - added special case values printing corectness check

--- tests/cli.nim
+++ tests/cli.nim
@@ -14,3 +14,26 @@ suite "test if correct output is given when nimplex is run in command line with 
         check exitCode == 0
         for i in 0..<reference.len:
             check outputLines[i] == reference[i]
+
+    test "generate small integer simplex grid (FIP 3 3) and print values to stdout":
+        let 
+            (output, exitCode) = execCmdEx("./nimplex -c FIP 3 3")
+            outputLines = output.splitLines
+            reference = @[
+                "Running with configuration:@[\"FIP\", \"3\", \"3\"]", 
+                "Full Output:Tensor[system.int] of shape \"[10, 3]\" on backend \"Cpu\"", 
+                "|0      0     3|", 
+                "|0      1     2|", 
+                "|0      2     1|", 
+                "|0      3     0|", 
+                "|1      0     2|", 
+                "|1      1     1|", 
+                "|1      2     0|", 
+                "|2      0     1|", 
+                "|2      1     0|", 
+                "|3      0     0|", 
+                "Full shape:[10, 3]"]
+        check exitCode == 0
+        for i in 0..<reference.len:
+            check outputLines[i] == reference[i]
+
