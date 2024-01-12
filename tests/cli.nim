9cd29f578e5aa93cc62defc817bfe48093146f67
amkrajewski <ak@psu.edu>
2024-01-12 11:12:45 +0100

    - added shape testing for internal simplex grids and standalone values testig from printout

--- tests/cli.nim
+++ tests/cli.nim
@@ -1,6 +1,8 @@
 import std/[os, osproc]
 import std/unittest
 import std/strutils
+import std/sequtils
+import std/sugar
 
 suite "test if correct output is given when nimplex is run in command line with some selected configurations":
     test "check if compiled nimplex is present in the current working directory":
@@ -54,4 +56,42 @@ suite "test if correct output is given when nimplex is run in command line with 
         check exitCode == 0
         for i in 0..<reference.len:
             check outputLines[i] == reference[i]
+
+    test "generate small integer internal simplex grid (IIP 3 5) and print values to stdout":
+        let 
+            (output, exitCode) = execCmdEx("./nimplex -c IIP 3 5")
+            outputLines = output.splitLines
+            reference = @[
+                "Running with configuration:@[\"IIP\", \"3\", \"5\"]", 
+                "Full Output:Tensor[system.int] of shape \"[6, 3]\" on backend \"Cpu\"", 
+                "|1      1     3|", 
+                "|1      2     2|", 
+                "|1      3     1|", 
+                "|2      1     2|", 
+                "|2      2     1|", 
+                "|3      1     1|", 
+                "Full shape:[6, 3]"]
+        check exitCode == 0
+        for i in 0..<reference.len:
+            check outputLines[i] == reference[i]
+
+    test "(significant if previous failes) test if values themself generated for small integer internal simplex grid (IIP 3 5) are correct":
+        let 
+            (output, exitCode) = execCmdEx("./nimplex -c IIP 3 5")
+            outputLines = output.splitLines
+            reference = @[
+                @[1, 1, 3],
+                @[1, 2, 2],
+                @[1, 3, 1],
+                @[2, 1, 2],
+                @[2, 2, 1],
+                @[3, 1, 1]]
+        check exitCode == 0
+        var outputGrid = newSeq[seq[int]](outputLines.len-3)
+        for i in 0..<reference.len:
+            for v in outputLines[i+2].replace("|","").splitWhitespace:
+                outputGrid[i].add(parseInt(v))
+
+        for i in 0..<reference.len:
+            check outputGrid[i] == reference[i]
 
