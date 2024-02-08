02b4fdd792f48d3dddcf61f10a8821347fdf9178
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-08 10:50:15 -0500

    - added a brand new Nim script for generating changelog from GitHub API response

---
+++ docs/getChangelog.nim
@@ -0,0 +1,38 @@
+import std/httpclient
+import std/json
+import std/streams
+import std/strutils
+import std/times
+
+const 
+    changelogType {.strdefine.}: string = "nim"
+    user {.strdefine.}: string = "amkrajewski"
+    repo {.strdefine.}: string = "nimplex"
+    
+assert changelogType == "md" or changelogType == "nim", "Invalid changelog type. Only 'md' and 'nim' are supported."
+
+let 
+    targetFile = newFileStream("changelog." & changelogType, fmWrite)
+    url: string = "https://api.github.com/repos/" & user & "/" & repo & "/releases"
+    response = newHttpClient().get(url)
+    data = parseJson(response.body)
+
+for d in data:
+    let 
+        tag = d["tag_name"].getStr()
+        url = d["html_url"].getStr()
+        createdDate = d["created_at"].getStr().parse("yyyy-MM-dd'T'HH:mm:ss'Z'", utc())
+        body = d["body"].getStr()
+
+    if changelogType == "md":
+        targetFile.writeLine("## " & "[" & tag & "]" & "(" & url & ") (" & createdDate.format("MMM d'st', yyyy") & ")")
+        targetFile.writeLine(body)
+        targetFile.writeLine("\n")
+
+    if changelogType == "nim":
+        targetFile.writeLine("## ## " & tag & " (" & createdDate.format("MMM d'st', yyyy") & ")")
+        for line in body.splitLines():
+            targetFile.writeLine("## " & line)
+        targetFile.writeLine("\n")
+
+targetFile.close()
