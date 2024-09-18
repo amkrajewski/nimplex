f47baf89b60aef942af3bc3c67af8355bf8549d8
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-09-18 15:31:17 -0400

    (stitching) improved entity naming by replacing domain-specific `system` with more general `space` in docs and names

--- utils/stitching.nim
+++ utils/stitching.nim
@@ -17,25 +17,25 @@ import ../nimplex
 
 func generateAlphabetSequence(length: int): seq[string] =
     ## Generates a sequence of strings of length `length` containing all UPPER and lower case letters of the alphabet. 
-    ## The primary purpose of this function is to generate unique names for system (A-B-C-D) and its ordered subsystems
+    ## The primary purpose of this function is to generate unique names for space (A-B-C-D) and its ordered subspaces
     ## (e.g., A-B, B-A) if no custom names are provided by the user.
-    assert length <= 52, "The number of characters requested to name system components exceeds the number of available symbols (52) - uppercase and lowercase letters)."
+    assert length <= 52, "The number of characters requested to name space components exceeds the number of available symbols (52) - uppercase and lowercase letters)."
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     for i in 0..<length:
         result.add($alphabet[i])
 
 
 func nonZeroComps(p: Tensor[int]): seq[int] =
-    ## Finds the indices of non-zero components in the input tensor `p` to determine which (unordered) system/subsystem it belongs to.
+    ## Finds the indices of non-zero components in the input tensor `p` to determine which (unordered) space/subspace it belongs to.
     for i in 0..<p.shape[0]:
         if p[i] != 0:
             result.add(i)
 
 
-func sys2name(sys: seq[int], components: seq[string]): string =
-    ## Converts the system/subsystem represented by the indices in `sys` to a string representation using the names of the `components`.
+func space2name(space: seq[int], components: seq[string]): string =
+    ## Converts the space/subspace represented by the indices in `space` to a string representation using the names of the `components`.
     ## It can be used in conjunction with `generateAlphabetSequence` too.
-    for i in sys:
+    for i in space:
         result &= components[i]
-        if i != sys[sys.len-1]:
+        if i != space[space.len-1]:
             result &= "-"
