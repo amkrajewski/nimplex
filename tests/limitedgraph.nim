72fbc2cc88cfe8f5f08d95638c129be676c170f3
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2025-04-10 13:57:28 -0400

    (tests) started implementing tests for the  new functionality of generating "limited" compositional graphs

---
+++ tests/limitedgraph.nim
@@ -0,0 +1,11 @@
+import std/unittest
+import ../nimplex
+import arraymancer/Tensor
+import std/sequtils
+import std/sugar
+import std/sets
+import std/times
+
+# SMALL GRAPHS
+let t0 = cpuTime()
+echo "***** SMALL TESTS *****"
