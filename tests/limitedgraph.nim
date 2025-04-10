e8d77a21066a4d4a83cab359c53f062846f697e7
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2025-04-10 13:59:57 -0400

    (tests) "limited" comp graph test of the simplest binary case with limit that can be easily conceptualized and verified

--- tests/limitedgraph.nim
+++ tests/limitedgraph.nim
@@ -44,3 +44,37 @@ suite "small (5-divisions) simplex integer 2-component (binary) graph with limit
         check neighbors == 
             @[@[1], @[0, 2], @[1, 3], @[2, 4], @[3, 5], @[4]]
 
+suite "small (5-divisions) simplex integer 2-component (binary) graph with limits with limits of [[0, 5], [0, 3]] so *limit on single (second) component*":
+    let 
+        nDiv:int = 5
+        limit:seq[seq[int]] = @[@[0, nDiv], @[0, 3]]
+        (nodes, neighbors) = nimplex.simplex_graph_limited(2, 5, limit)
+        neighborsNumbers: seq[int] = neighbors.map(n => n.len)
+        edgesCount = neighborsNumbers.foldl(a+b)
+    
+    echo "Nodes:\n", nodes
+    echo "Neighbors:\n", neighbors
+
+    test "correct dimensionality of nodes/vertices":
+        check nodes.shape[1] == 2
+
+    test "correct number of nodes/vertices":
+        check nodes.shape[0] == 4
+
+    test "correct number of neighbors (graph edges)":
+        check edgesCount == 6
+
+    test "correct maximum number of neighbors":
+        check neighborsNumbers.max == 2*(2-1)
+
+    test "correct minimum number of neighbors":
+        check neighborsNumbers.min == 1
+
+    test "correct node/vertex positions in the simplex":
+        check nodes.toSeq2D() == 
+            @[@[2, 3], @[3, 2], @[4, 1], @[5, 0]]
+
+    test "correct neighbors list for each node/vertex":
+        check neighbors == 
+            @[@[1], @[0, 2], @[1, 3], @[2]]
+
